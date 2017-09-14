99f8a25693396cbc3ac03917ce3ff0a1e8678410
Max Audron <audron@synmatter.com>
2017-09-14 19:02:29 +0200

    reworked auth structure finalised get method created post method everything working right now TODO: better error handling

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from flask import jsonify
 from flask import g
 from flask import request
 from flask_httpauth import HTTPBasicAuth
-
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
-
-
 import ts3
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+# Config
+app.config['SECRET_KEY'] = 'a long secret to encrypt user data' # Secret used for generation of auth token PLEASE CHANGE
+
 class User():
     # ...
 
     def generate_auth_token(userdata, ip, expiration = 6000):
-        s = Serializer('SECRET_KEY', expires_in = expiration)
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
         return s.dumps({ 'username': userdata['username'], 'password': userdata['password'], 'ip': ip }) # creates token
 
     @staticmethod
     def verify_auth_token(token):
-        s = Serializer('SECRET_KEY')
+        s = Serializer(app.config['SECRET_KEY'])
         try:
             data = s.loads(token)  # decrypts the token
         except SignatureExpired:
@@ -39,9 +39,8 @@ def get_auth_token():
 
 @auth.verify_password
 def verify_password(username, password):
-    user = User.verify_auth_token(username) # checks if auth method is token based and validates the token if yes
-    if not user:                            # if not token based uses password auth
-        print('password auth')
+    if request.path == '/api/auth':              # if user requests token use password auth
+        print('generating token')
         with ts3.query.TS3Connection(request.headers['ip']) as ts3conn: # connect to teamspeak server
             try:
                 ts3conn.login(client_login_name=username, client_login_password=password) # login to ts and check if valid
@@ -51,23 +50,54 @@ def verify_password(username, password):
             except ts3.query.TS3QueryError as err:# shows error message if login fails
                 print("Login failed:", err.resp.error["msg"])
                 return False
+            print("Error:", resp.error["id"], resp.error["msg"])
+    else:
+        user = User.verify_auth_token(username)
+        if not user:
+            return False
     g.userdata = user
     return True
 
-@app.route('/api/clientlist')
+@app.route('/api/get/<command>')
 @auth.login_required
-def index():
+def get(command):
     with ts3.query.TS3Connection(g.userdata['ip']) as ts3conn:
         try:
             ts3conn.login(client_login_name=g.userdata['username'], client_login_password=g.userdata['password'])
 
         except ts3.query.TS3QueryError as err:
-            print("Login failed:", err.resp.error["msg"])
+            return jsonify("Login failed:", err.resp.error["msg"])
             exit(1)
 
-        ts3conn.use(sid=2)
-        res = ts3conn.clientlist()
-        print(res.parsed)
+        ts3conn.use(sid=request.headers['sid'])
+        try:
+            func = getattr(ts3conn, command)
+        except AttributeError:
+            return jsonify({'message': 'Command not found: {0}' .format(command) })
+        else:
+            res = func()
+        return jsonify(res.parsed)
+
+@app.route('/api/post/<command>', methods=['POST'])
+@auth.login_required
+def post(command):
+    with ts3.query.TS3Connection(g.userdata['ip']) as ts3conn:
+        print(request.get_json())
+        req = request.get_json()
+        try:
+            ts3conn.login(client_login_name=g.userdata['username'], client_login_password=g.userdata['password'])
+
+        except ts3.query.TS3QueryError as err:
+            return jsonify("Login failed:", err.resp.error["msg"])
+            exit(1)
+
+        ts3conn.use(sid=request.headers['sid'])
+        try:
+            func = getattr(ts3conn, command)
+        except AttributeError:
+            return jsonify({'message': 'Command not found: {0}' .format(command) })
+        else:
+            res = func(**req)
         return jsonify(res.parsed)
 
 if __name__ == '__main__':
